cd9bc5104ade04f09185f2ded358d69a791f28d8
mpicca83 <mpicca83@hotmail.com>
2023-11-15 21:50:48 -0300

    Primera Entrega

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Mi Primer Proyecto en Desarrollo de Aplicaciones</Text>
+      <Text>Mauricio Picca</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +16,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
